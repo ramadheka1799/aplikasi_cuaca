405fc810306d485ee399fd2c27ee13f583afe835
ramadheka1799 <102200976+ramadheka1799@users.noreply.github.com>
2022-05-16 23:39:34 +0800

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\belajarflutter\\cuaca_app" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=C:\\belajarflutter\\cuaca_app"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\belajarflutter\\cuaca_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\belajarflutter\\cuaca_app"
+  "FLUTTER_TARGET=C:\\belajarflutter\\cuaca_app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\belajarflutter\\cuaca_app\\.dart_tool\\package_config.json"
+)
